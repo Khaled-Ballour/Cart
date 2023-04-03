b1a99c7716c8b1198b99b6bf2405f660bca5cdb2
unknown <ballourk@gmail.com>
2023-04-03 16:36:45 +0300

    Fix the context error

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,9 +3,7 @@ import { createContext, useContext, useEffect, useReducer } from 'react';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
-  );
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => {
