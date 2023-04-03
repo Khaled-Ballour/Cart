5b902a42ad62171db67d4953cb41789f3a559789
unknown <ballourk@gmail.com>
2023-04-03 16:52:24 +0300

    built the reducer

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,9 +1,26 @@
 import { createContext, useContext, useEffect, useReducer } from 'react';
+import reducer from './reducer';
+import {
+  CLEAR_CART,
+  INCREASE,
+  DECREASE,
+  REMOVE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from './actions';
 
 const AppContext = createContext();
 
+const initialState = {
+  loading: false,
+  cart: [],
+};
+
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+import {
+  CLEAR_CART,
+  INCREASE,
+  DECREASE,
+  REMOVE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from './actions';
+
+const reducer = (state, action) => {
+  return state;
+};
+
+export default reducer;
