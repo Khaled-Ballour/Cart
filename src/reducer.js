76eaec3462ca6399f98d83d70be390ca90f80199
unknown <ballourk@gmail.com>
2023-04-03 18:45:03 +0300

    remove functionality

--- src/reducer.js
+++ src/reducer.js
@@ -11,6 +11,12 @@ const reducer = (state, action) => {
   if (action.type === CLEAR_CART) {
     return { ...state, cart: new Map() };
   }
+  if (action.type === REMOVE) {
+    const newCart = new Map(state.cart);
+    newCart.delete(action.payload.id);
+    console.log(newCart);
+    return { ...state, cart: newCart };
+  }
   throw new Error(`No matching action type: ${action} `);
 };
 
